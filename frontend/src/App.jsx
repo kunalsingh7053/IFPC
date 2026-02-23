58edd49114cc178b25ecdc0418808bf6d8cf0779
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2026-02-23 16:40:59 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold text-red-500">IFPC</h1>
+    </div>
+  )
+}
+
+export default App
