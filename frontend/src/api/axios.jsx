412676286bbb99be21bb71579c587246da424c95
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2026-02-23 23:01:26 +0530

    create enviroment

---
+++ frontend/src/api/axios.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://translator-lo1e.onrender.com/api", // backend base URL
+  withCredentials: true, // send cookies automatically 
+});
+
+export default API;
